76edd6cfd1cf59e50d948853022d667c0b7ce777
Rasmus Kramare <rasmus.kramare@live.se>
2021-12-10 16:12:57 +0100

    Day 9a.

--- aoc09.py
+++ aoc09.py
@@ -9,7 +9,7 @@ def get_inputs(path):
 def is_in_bounds(height_map, row, col):
     row_max = len(height_map)
     col_max = len(height_map[0])
-    return row > 0 and row < row_max and col > 0 and col < col_max
+    return row >= 0 and row < row_max and col >= 0 and col < col_max
 
 
 def get_point(heigth_map, row, col):
@@ -22,32 +22,34 @@ def is_lowpoint(height_map, row, col):
     col_left = col - 1
     col_right = col + 1
 
-    print("r, u, d", row, row_up, row_down)
-    print("c, l, r", col, col_left, col_right)
+    #print("r, u, d", row, row_up, row_down)
+    #print("c, l, r", col, col_left, col_right)
 
     current_point = height_map[row][col]# get_point(height_map, row, col)
-    print("Doing:", current_point, row, col)
+    #print("Doing:", current_point, row, col)
 
     if is_in_bounds(height_map, row_up, col):
-        if current_point > height_map[row_up][col]:
-            print("up", current_point, height_map[row_up][col], row_up, col)
+        if current_point >= height_map[row_up][col]:
+            #print("up", current_point, height_map[row_up][col], row_up, col)
             return False
 
     if is_in_bounds(height_map, row_down, col):
-        if current_point > height_map[row_down][col]:
-            print("down", current_point, height_map[row_down][col], row_down, col)
+        if current_point >= height_map[row_down][col]:
+            #print("down", current_point, height_map[row_down][col], row_down, col)
             return False
     
     if is_in_bounds(height_map, row, col_left):
-        if current_point > height_map[row][col_left]:
-            print("left", current_point, height_map[row][col_left], row, col_left)
+        if current_point >= height_map[row][col_left]:
+            #print("left", current_point, height_map[row][col_left], row, col_left)
             return False
     
     if is_in_bounds(height_map, row, col_right):
-        if current_point > height_map[row][col_right]:
-            print("right", current_point, height_map[row][col_left], row, col_right)
+        if current_point >= height_map[row][col_right]:
+            #print("right", current_point, height_map[row][col_left], row, col_right)
             return False
-    print("Adding:", current_point, row, col)
+
+    #print(f"up {height_map[row_up][col]} down {height_map[row_down][col]} left {height_map[row][col_left]} right {height_map[row][col_right]}")
+    #print("Adding:", current_point, row, col)
     
     return True
 
@@ -58,11 +60,12 @@ def get_a(height_map):
             if is_lowpoint(height_map, row, col):
                 lowpoints.append(height_map[row][col])
     print(lowpoints)
+    return sum([s + 1 for s in lowpoints])
     
 if __name__ == "__main__":
     test_input = get_inputs("test/test09")
-    print(test_input)
-    get_a(test_input)
+    inputs = get_inputs("inputs/day09")
+    #print(test_input)
+    print(get_a(inputs))
 
-    #inputs = get_inputs("inputs/day09")
     
